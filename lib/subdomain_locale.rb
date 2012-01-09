257816689bb731a014d84dec78de9581cff47acd
Semyon Perepelitsa <sema@sema.in>
2012-01-10 04:02:18 +0800

    Add railtie

--- lib/subdomain_locale.rb
+++ lib/subdomain_locale.rb
@@ -1,6 +1,9 @@
+require "subdomain_locale/railtie" if defined?(Rails)
+
 module SubdomainLocale
   VERSION = "0.0.2"
   autoload :UrlFor,     "subdomain_locale/url_for"
   autoload :Controller, "subdomain_locale/controller"
   autoload :Locale,     "subdomain_locale/locale"
+  autoload :Railtie,    "subdomain_locale/railtie"
 end
---
+++ lib/subdomain_locale/railtie.rb
@@ -0,0 +1,15 @@
+module SubdomainLocale
+  class Railtie < ::Rails::Railtie
+
+    initializer "subdomain_locale.url_helpers" do
+      Rails.application.routes.url_helpers.send :include, SubdomainLocale::UrlFor
+    end
+
+    initializer "subdomain_locale.controller" do
+      ActiveSupport.on_load :action_controller do
+        include SubdomainLocale::Controller
+      end
+    end
+
+  end
+end
