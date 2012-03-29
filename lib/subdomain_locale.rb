b404df22a996b5483fd60dea3a674e5f9d377ef8
Semyon Perepelitsa <sema@sema.in>
2012-03-30 01:39:08 +0800

    Move version into a separate file

--- lib/subdomain_locale.rb
+++ lib/subdomain_locale.rb
@@ -1,5 +1,2 @@
+require "subdomain_locale/version"
 require "subdomain_locale/railtie" if defined?(Rails)
-
-module SubdomainLocale
-  VERSION = "0.0.3"
-end
---
+++ lib/subdomain_locale/version.rb
@@ -0,0 +1,3 @@
+module SubdomainLocale
+  VERSION = "0.0.3"
+end
