673a4f839e628b57d4f9f6dfe717c878a62edd17
Semyon Perepelitsa <sema@sema.in>
2012-03-30 01:53:45 +0800

    Add documentation

--- lib/subdomain_locale/url_for.rb
+++ lib/subdomain_locale/url_for.rb
@@ -2,6 +2,13 @@ require "subdomain_locale/locale"
 
 module SubdomainLocale
   module UrlFor
+    # Makes url_for(locale: 'ru') the same as url_for(subdomain: 'ru', only_path: false)
+    # That way you can easily swap locale in subdomain with locale in the path.
+    #
+    # E. g. assuming you have <tt>scope ":locale"</tt> in your routes:
+    #   url_for params.merge(locale: 'ru') # => /ru/current_path
+    # After including this module:
+    #   url_for params.merge(locale: 'ru') # => http://ru.example.com/current_path
     def url_for(options=nil)
       if options.is_a?(Hash) and locale = options.delete(:locale)
         options[:subdomain] = Locale.new(locale).subdomain
