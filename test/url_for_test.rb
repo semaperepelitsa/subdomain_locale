b2e0bb0ade23e3afaea6df29f440bd8bb51d4618
Semyon Perepelitsa <sema@sema.in>
2012-03-30 01:54:36 +0800

    Set ivar outside the method

--- test/url_for_test.rb
+++ test/url_for_test.rb
@@ -5,7 +5,7 @@ require "subdomain_locale/url_for"
 class UrlForTest < MiniTest::Unit::TestCase
   module UrlFor
     def url_for(*args)
-      @actual = args
+      args
     end
   end
 
@@ -13,27 +13,27 @@ class UrlForTest < MiniTest::Unit::TestCase
   include SubdomainLocale::UrlFor
 
   def test_does_not_affect_if_there_is_no_locale
-    url_for(foo: 'bar')
+    @actual = url_for(foo: 'bar')
     assert_equal [{foo: 'bar'}], @actual
   end
 
   def test_does_not_affect_string_argument
-    url_for('/bar')
+    @actual = url_for('/bar')
     assert_equal ['/bar'], @actual
   end
 
   def test_replaces_locale_with_subdomain_and_forces_not_only_path
-    url_for(foo: 'bar', locale: :ru)
+    @actual = url_for(foo: 'bar', locale: :ru)
     assert_equal [{foo: 'bar', subdomain: 'ru', only_path: false}], @actual
   end
 
   def test_replaces_locale_with_subdomain_and_overrides_only_path
-    url_for(foo: 'bar', locale: :ru, only_path: true)
+    @actual = url_for(foo: 'bar', locale: :ru, only_path: true)
     assert_equal [{foo: 'bar', subdomain: 'ru', only_path: false}], @actual
   end
 
   def test_sets_subdomain_to_www_for_default_locale
-    url_for(foo: 'bar', locale: :az)
+    @actual = url_for(foo: 'bar', locale: :az)
     assert_equal [{foo: 'bar', subdomain: 'www', only_path: false}], @actual
   end
 end
