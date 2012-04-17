cf025262747e3e63abb080a2ce89f84c96c5610f
Semyon Perepelitsa <sema@sema.in>
2012-04-17 18:38:06 +0800

    Move requires into the initialization hooks.

--- lib/subdomain_locale/railtie.rb
+++ lib/subdomain_locale/railtie.rb
@@ -1,16 +1,16 @@
 require "rails/railtie"
-require "subdomain_locale/url_for"
-require "subdomain_locale/controller"
 
 module SubdomainLocale
   class Railtie < ::Rails::Railtie
 
     initializer "subdomain_locale.url_helpers" do
+      require "subdomain_locale/url_for"
       Rails.application.routes.url_helpers.send :include, SubdomainLocale::UrlFor
     end
 
     initializer "subdomain_locale.controller" do
       ActiveSupport.on_load :action_controller do
+        require "subdomain_locale/controller"
         include SubdomainLocale::Controller
       end
     end
