1c5f745a39a0da227d1a09ff76081b5e17556b97
Semyon Perepelitsa <sema@sema.in>
2014-02-15 16:18:36 +0800

    Merge both rails integration tests into one file.

--- test/rails_test.rb
+++ test/rails_test.rb
@@ -3,6 +3,7 @@ ENV["RAILS_ENV"] = "test"
 
 require "bundler/setup"
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
+require "minitest/autorun"
 require "rails/test_help"
 Rails.backtrace_cleaner.remove_silencers!
 
@@ -44,5 +45,12 @@ class HelloControllerTest < ActionController::TestCase
     get :world
     assert_response :ok
     assert_select "p", "Hello"
+  end
+end
+
+class HelloMailerTest < ActionController::TestCase
+  def test
+    mail = I18n.with_locale(:ru) { HelloMailer.world }
+    assert_equal "http://ru.example.com/", mail.body.to_s.lines[3].chomp
   end
 end
