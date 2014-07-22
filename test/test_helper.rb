4500a151bae310d3ff06780b2845ca65b4179fc4
Semyon Perepelitsa <sema@sema.in>
2014-07-22 07:07:47 +0400

    Enable Ruby warnings when testing.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,3 +5,10 @@ require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require "minitest/autorun"
 require "rails/test_help"
 Rails.backtrace_cleaner.remove_silencers!
+
+# Mail 2.5 gives warnings, fixed in 2.6
+require "mail"
+Mail::Sendmail
+##
+
+$VERBOSE = true
